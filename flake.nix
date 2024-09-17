054eddc8bbb1ffee8e3b44efd8d0803be0087bb0
coenicorn <coenicorn.blauwzee@gmail.com>
2024-09-17 15:11:06 +0200

    changes

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
-      hostName = "nixos" {
+      coenicorn-nix-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./configuration.nix ];
      };
